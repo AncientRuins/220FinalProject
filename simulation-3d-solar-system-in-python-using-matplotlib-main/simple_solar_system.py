7b12e4059b57b9b8f2c0af5b458160b09a98547a
AncientRuins <nishwanthg@hotmail.com>
2023-05-05 19:09:19 -0400

    Corrected Issue with Input Reception

--- simulation-3d-solar-system-in-python-using-matplotlib-main/simple_solar_system.py
+++ simulation-3d-solar-system-in-python-using-matplotlib-main/simple_solar_system.py
@@ -3,6 +3,8 @@
 from solar_system_3d import SolarSystem, Sun, Planet
 import time
 
+#This is how to create a solar system programatically
+'''
 solar_system = SolarSystem(400, projection_2d=True)
 
 sun = Sun(solar_system)
@@ -25,9 +27,10 @@ while True:
     solar_system.calculate_all_body_interactions()
     solar_system.update_all()
     solar_system.draw_all()
-    
+    '''
 
-'''n = int(input("How many celestial bodies do you want to add: "))
+#This is how to create a solar system via the console
+n = int(input("How many celestial bodies do you want to add: "))
 print(n)
 
 solar_system = SolarSystem(400, projection_2d=True)
@@ -35,19 +38,19 @@ t = 0.0
     
 
 for i in range(0, n):
-    print("\nNow requesting the information about object ", str(i+1), "!\n")
-    time.sleep(0.001)
-    mass = float(input("What is the mass of object (in solar masses): "))
+    mass = float(input("\nNow requesting information about the next object.\nWhat is the mass of the object (in solar masses): "))
     xpos = float(input("What is the location in the x-direction (in millions of km): "))
     ypos = float(input("What is the location in the y-direction (in millions of km): "))
     zpos = float(input("What is the location in the z-direction (in millions of km): "))
     xvel = float(input("What is the velocity in the x-direction (in millions of km / year): "))
     yvel = float(input("What is the velocity in the y-direction (in millions of km / year): "))
     zvel = float(input("What is the velocity in the z-direction (in millions of km / year): "))
+    print(mass, " ", xpos, " ", ypos, " ", zpos, " ", xvel, " ", yvel, " ", zvel)
     Planet(solar_system, mass=mass, position=(xpos, ypos, zpos), velocity=(xvel, yvel, zvel))
     
+
 if n > 0:
     while True:
         solar_system.calculate_all_body_interactions()
         solar_system.update_all()
-        solar_system.draw_all()'''
+        solar_system.draw_all()
